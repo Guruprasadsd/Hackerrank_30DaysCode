0134b44b6dfd1201045f650832b37b58812f98e3
Guruprasad Davangave <49909099+Guruprasadsd@users.noreply.github.com>
2022-01-23 10:50:28 +0530

    Regex

--- Day28-Regex.py
+++ Day28-Regex.py
@@ -1,3 +1,27 @@
 '''
 https://www.hackerrank.com/challenges/30-regex-patterns?isFullScreen=true
 '''
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    N = int(input().strip())
+    validNamesList=[]
+    for N_itr in range(N):
+        first_multiple_input = input().rstrip().split()
+
+        firstName = first_multiple_input[0]
+
+        emailID = first_multiple_input[1]
+        if emailID.find("@gmail.com") != -1: #gmail email found
+            validNamesList.append(firstName)
+    for i in sorted(validNamesList):
+        print(i)
+
