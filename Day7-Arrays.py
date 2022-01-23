9aea678cce98d3d7241c93bd677104ec2c94a7c0
Guruprasad Davangave <49909099+Guruprasadsd@users.noreply.github.com>
2022-01-23 11:48:40 +0530

    Arrays

--- Day7-Arrays.py
+++ Day7-Arrays.py
@@ -1,3 +1,20 @@
 '''
 https://www.hackerrank.com/challenges/30-arrays?isFullScreen=true
 '''
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+    res=arr[::-1]
+    for i in res:
+        print(i,end=" ")
