0aba47a896eaef1d50deaccd156e399dc4bf2a68
Guruprasad Davangave <49909099+Guruprasadsd@users.noreply.github.com>
2022-01-23 11:50:42 +0530

    Loops

--- Day5-Loops.py
+++ Day5-Loops.py
@@ -1,3 +1,18 @@
 '''
 https://www.hackerrank.com/challenges/30-loops?isFullScreen=true
 '''
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+T=int(input())
+for i in range(0,T):
+    S=input()
+    print(S[0::2],S[1::2])
+    
+    
+#for j in m.split("\n"):
+    #print(m[0:len(m):2],m[1:len(m):2])
+"""
+    T = int(input())
+for i in range (0 , T):
+    S = input()
+    print(S[0::2] + " " + S[1::2])
+    """
