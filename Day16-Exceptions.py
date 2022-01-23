b25e76ae2085c61db319b4bcc3af8ce08694d094
Guruprasad Davangave <49909099+Guruprasadsd@users.noreply.github.com>
2022-01-23 11:38:46 +0530

    Exceptions

--- Day16-Exceptions.py
+++ Day16-Exceptions.py
@@ -1,3 +1,16 @@
 '''
 https://www.hackerrank.com/challenges/30-exceptions-string-to-integer?isFullScreen=true
 '''
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+try:
+    print(int(input()))
+except ValueError:
+    print("Bad String")
