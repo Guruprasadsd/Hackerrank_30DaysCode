efdeca973ebc6f81fa18075d496a43a08dbe30d4
Guruprasad Davangave <49909099+Guruprasadsd@users.noreply.github.com>
2022-01-23 10:53:12 +0530

    Running Time and Complexity

--- Day25-RunningTime.py
+++ Day25-RunningTime.py
@@ -1,3 +1,24 @@
 '''
 https://www.hackerrank.com/challenges/30-running-time-and-complexity?isFullScreen=true
 '''
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+from math import sqrt
+from sys import stdin
+
+
+def checkPrime(n):
+    for i in range(2, int(sqrt(n))+1):
+        if n % i is 0:
+            return False
+    return True
+
+
+n = int(input())
+for line in stdin:
+    val = int(line)
+    if (val >= 2 and checkPrime(val)):
+        print("Prime")
+    else:
+        print("Not prime")
+
+
