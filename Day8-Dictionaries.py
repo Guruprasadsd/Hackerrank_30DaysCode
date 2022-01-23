1a93404084926053b81176990b89691470952e0e
Guruprasad Davangave <49909099+Guruprasadsd@users.noreply.github.com>
2022-01-23 11:47:23 +0530

    Dictionaries

--- Day8-Dictionaries.py
+++ Day8-Dictionaries.py
@@ -1,3 +1,18 @@
 '''
 https://www.hackerrank.com/challenges/30-dictionaries-and-maps?isFullScreen=true
 '''
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+import sys
+n = int(input())
+phoneBook = {}
+for i in range(n):
+    contact = input().split(' ')
+    phoneBook[contact[0]] = contact[1]
+# Process Queries
+lines = sys.stdin.readlines()
+for i in lines:
+    name = i.strip()
+    if name in phoneBook:
+        print(name + '=' + str( phoneBook[name] ))
+    else:
+        print('Not found')
