915f41c72b76b6bd96341555d3c5b75ca56564f7
Guruprasad Davangave <49909099+Guruprasadsd@users.noreply.github.com>
2022-01-23 11:45:57 +0530

    Binary Numbers

--- Day10-BinaryNumbers.py
+++ Day10-BinaryNumbers.py
@@ -1,3 +1,22 @@
 '''
 https://www.hackerrank.com/challenges/30-binary-numbers?isFullScreen=true
 '''
+#!/bin/python3
+
+num = int(input())
+
+result = 0
+max1 = 0
+
+while num > 0:
+    if num % 2 == 1:
+        result += 1
+        if result > max1:
+            max1 = result
+
+    else:
+        result = 0
+
+    num //= 2
+
+print(max1)
