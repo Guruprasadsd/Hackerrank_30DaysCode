0162919c211f72dfd3e5f7bd3fe5c86fd1f893ad
Guruprasad Davangave <49909099+Guruprasadsd@users.noreply.github.com>
2022-01-23 11:57:40 +0530

    HelloWorld

---
+++ Day0-HelloWorld.py
@@ -0,0 +1,10 @@
+'''
+https://www.hackerrank.com/challenges/30-hello-world/problem?isFullScreen=true
+'''
+inputString = raw_input() # get a line of input from stdin and save it to our variable
+
+# Your first line of output goes here
+print 'Hello, World.'
+
+# Write the second line of output
+print(inputString)
